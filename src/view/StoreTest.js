5c3edd633daee03260c66e19c0ae7de4c6642a7f
allen <ywl_allen@163.com>
2020-12-13 23:22:06 +0800

    store done

--- src/view/StoreTest.js
+++ src/view/StoreTest.js
@@ -8,57 +8,88 @@ import {
 } from "@sencha/ext-react-modern";
 
 class Keyword extends React.Component {
-  model = {
+  chipviewModel = {
     data: [
       {
-        keywordKey: "abc",
+        chipKeywordKey: "Thor",
       },
       {
-        keywordKey: "bcd",
+        chipKeywordKey: "Caption America",
       },
       {
-        keywordKey: "cde",
+        chipKeywordKey: "Iron man",
+      },
+      {
+        chipKeywordKey: "Son Goku",
+      },
+      {
+        chipKeywordKey: "Son Gohan",
       },
     ],
   };
-  collapse = true;
-  SUFFIX = " (New Keyword)";
+
+  maxChip = 7; // 最大标签数
+  showNumber = 5; // 列表显示条数，不包含输入时自动加入新记录
+  loadDataLength = 3; // 输入字符长度达到后开始加载数据
+  SUFFIX = " (New Keyword)"; // 输入新增记录的后缀
 
   comboxModel = {
     data: [
       {
-        keywordKey: "A1fadfdsf",
+        comboxKeywordKey: "Thor",
       },
       {
-        keywordKey: "B1dddaaa",
+        comboxKeywordKey: "Caption America",
+      },
+
+      {
+        comboxKeywordKey: "Iron man",
       },
       {
-        keywordKey: "C1sfsadfsf",
+        comboxKeywordKey: "Son Goku",
       },
       {
-        keywordKey: "D1zzxcvxcv",
-      },
-      {
-        keywordKey: "E1kjhkhj",
-      },
-      {
-        keywordKey: "F1dafsdf",
+        comboxKeywordKey: "Son Gohan",
       },
     ],
   };
 
-  store1 = new Ext.data.Store(this.model);
-  store2 = new Ext.data.Store(this.model);
+  // 将 store 里的数据转为 js 方便操作的数组
+  storeToArray(store) {
+    return store && store.getData().items.map((item) => item.data);
+  }
+
+  noDuplicate = (content) => {
+    return !this.storeToArray(this.store2).some(
+      (item) => item.chipKeywordKey === content
+    );
+  };
+
+  store1 = new Ext.data.Store(this.chipviewModel);
+  store2 = new Ext.data.Store(this.chipviewModel);
 
   comboxStore = new Ext.data.Store({ data: [] });
+
+  componentDidMount() {
+    // 检测 store 变化
+    this.store2.onEndUpdate = (e) => {
+      if (this.store2.count() >= this.maxChip) {
+        this.myCombox.clearValue();
+        this.myCombox.setDisabled(true);
+      } else {
+        this.myCombox.setDisabled(false);
+      }
+    };
+  }
 
   render() {
     return (
       <ExtContainer layout="vbox" padding={10}>
         <ExtChipview
-          id="chip1"
+          style={{ background: "yellowgreen" }}
+          padding={10}
           store={this.store1}
-          displayField={"keywordKey"}
+          displayField={"chipKeywordKey"}
           closeHandler={(chipview, location) => {
             chipview.store.remove(location.record);
 
@@ -68,11 +99,15 @@ class Keyword extends React.Component {
           }}
         />
         <ExtChipview
-          id="chip2"
+          style={{ background: "pink" }}
+          padding={10}
           store={this.store2}
-          displayField={"keywordKey"}
+          displayField={"chipKeywordKey"}
+          ripple={false}
           closeHandler={(chipview, location) => {
             chipview.store.remove(location.record);
+            // this.myCombox.clearValue();
+            // console.log(this.myCombox.setValue);
 
             // Stop the event, otherwise the NavigationModel
             // will try to click on the nonexistent Chip
@@ -80,97 +115,82 @@ class Keyword extends React.Component {
           }}
         />
 
-        <ExtButton
-          text="update chipview"
-          handler={() => {
-            const query = this.store1.query("keywordKey", "b", true);
-            const record = this.store1.findRecord("keywordKey", "b", 0, true);
-            console.log(query);
-            console.log(record);
-            this.store2.setRecords(record);
-          }}
-        ></ExtButton>
-        <ExtButton
-          text="set chipview"
-          handler={() => {
-            this.store2.setData([
-              {
-                keywordKey: "11",
-              },
-              {
-                keywordKey: "22",
-              },
-              {
-                keywordKey: "33",
-              },
-            ]);
-          }}
-        ></ExtButton>
-
         <ExtCombobox
           width={"100%"}
-          label="State"
           store={this.comboxStore}
-          displayField="keywordKey"
+          displayField="comboxKeywordKey"
           queryMode="local"
-          labelAlign="placeholder"
           anyMatch
-          //   queryDelay={1000}
+          clearable
           ref={(myCombox) => {
             this.myCombox = myCombox.cmp;
           }}
-          onCollapse={(event) => {
-            console.log("Collapse", event);
-            const value = event.field._inputValue;
+          itemTpl={"<div>{comboxKeywordKey}{suffix}</div>"}
+          onBlur={(event) => {
+            console.log("onBlur", this.canAdd);
 
-            // 后缀无需加入，去除后缀
-            if (value && value.indexOf(this.SUFFIX) > -1) {
-              console.log("kkkkk", value.replace(this.SUFFIX, ""));
-              //   this.myCombox.setValue(value.replace(this.SUFFIX, ""));
-              this.myCombox.inputElement.dom.value = value.replace(
-                this.SUFFIX,
-                ""
-              );
-            } else {
-              // this.myCombox.setValue(newValue);
-            }
+            this.canAdd = false;
+          }}
+          onFocus={(event) => {
+            this.canAdd = false;
+            console.log("focus", this.canAdd);
+          }}
+          // 下拉菜单关闭
+          onCollapse={(event) => {
+            console.log("onCollapse", this.canAdd);
+            this.canAdd = true;
+          }}
+          // 该事件当输入和选中下拉项都会触发，因此要作判断
+          onSelect={(event) => {
+            // 不同交互下 onCollapse 和 onSelect 触发顺序不一样，因此需要使用 setTimeout
+            window.setTimeout(() => {
+              console.log("onSelect", this.canAdd);
+
+              const { comboxKeywordKey } = event.newValue.data;
+
+              if (this.canAdd && this.noDuplicate(comboxKeywordKey)) {
+                this.store2.add({
+                  chipKeywordKey: comboxKeywordKey,
+                });
+                this.myCombox.setValue(comboxKeywordKey);
+              }
+              this.canAdd = false;
+            });
           }}
           onChange={(event) => {
-            console.log("onChange...", event);
-            const newValue = event.newValue;
-            if (newValue.trim().length > 4) {
-              this.comboxStore.setData(this.comboxModel.data);
+            const trimInput = event.newValue.trim();
 
-              let newData = this.comboxStore
-                .getData()
-                .items.map((item) => item.data);
-              console.log(newData);
-              newData = newData.slice(0, 5);
+            console.log("onchange", trimInput);
+            // 输入字符超过 loadDataLength 开始加载数据
+            if (trimInput.length > this.loadDataLength) {
+              this.comboxStore.setData(this.comboxModel.data); // 加载数据
 
-              newData.push({
-                keywordKey: `${newValue}${this.SUFFIX}`, // onChange
-              });
+              // 获取 store 里的数据，转换成数组
+              let newData = this.storeToArray(this.comboxStore);
+
+              // 只显示 showNumber 条加一条新记录
+              newData = newData.slice(0, this.showNumber);
+
+              if (this.noDuplicate(trimInput)) {
+                newData.push({
+                  comboxKeywordKey: trimInput,
+                  suffix: this.SUFFIX,
+                });
+              }
+
               this.comboxStore.setData(newData);
+            }
+            // trimInput 有值则代表输入触发该事件，空值则代表没有点击下拉项
+            if (trimInput) {
+              this.canAdd = false;
             }
           }}
         />
         <ExtButton
-          text="update combox"
+          text="save all keyword"
           handler={() => {
-            // this.comboxStore.setData([
-            //   {
-            //     keywordKey: "new A1",
-            //     comboxValue: "new VA1",
-            //   },
-            //   {
-            //     keywordKey: "new B1",
-            //     comboxValue: "new VB1",
-            //   },
-            //   {
-            //     keywordKey: "new C1",
-            //     comboxValue: "new VC1",
-            //   },
-            // ]);
+            const query = this.store2.query("chipKeywordKey", "", true);
+            this.store1.setRecords(query.items);
           }}
         ></ExtButton>
       </ExtContainer>
